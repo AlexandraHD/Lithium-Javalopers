8e1e3496ffa772b58b5e844334a84c78f63046d8
Hackathon <ktyneth@hotmail.com>
2023-06-28 09:09:31 -0500

    carrito

---
+++ Js/carrito.js
@@ -0,0 +1,167 @@
+
+/* -------------------------------------------------------------------------- */
+//^                funcion renderizar los productos del catalogo               */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+let products_container = document.querySelector('.discount');
+let productos_str = ''
+//funcion renderizar los productos del catalogo
+const renderizarCatalogo = (array) => {
+  array.forEach(producto => {
+    const product_card = document.createElement('div')
+    product_card.classList.add('product-container')
+    productos_str = `
+      <img src="${producto.imagen}" alt="${producto.alt}" class="img-item">
+      <span class="titulo-item">${producto.titulo}</span>
+      <strong class="precio-item">$ ${producto.precio}</strong>
+      <button class="button-ver">Detalles</button>
+      <button class="button-add">Comprar</button>
+      <button class="button-heart" onclick='' ><i class="fa-regular fa-heart"></i></button>
+      <button class="button-car" onclick='agregaraCarrito(${producto.id})'  data-id="1"><i class="fa-solid fa-cart-shopping"></i></i></button>
+    `
+    product_card.innerHTML = productos_str;
+    products_container.append(product_card)
+  });
+}
+renderizarCatalogo(productos)
+/* -------------------------------------------------------------------------- */
+
+
+/* ---------------------- mostrar y ocultar el carrito ---------------------- */
+
+const hidden = (elemento) => {
+  return elemento.classList.toggle('hidden')
+}
+
+// selecionas los elementos
+let btn_carrito = document.querySelector('#header-carrito__btn');
+const cart_products = document.querySelector('.container-carrito');
+
+//creas el evento
+btn_carrito.addEventListener("click", () => {
+  hidden(cart_products)
+  
+});
+
+/* -------------------------------------------------------------------------- */
+function existeObjetoConID(array, id) {
+  const objetosConID = array.filter(objeto => objeto.id === id);
+  return objetosConID.length > 0;
+}
+
+
+let carritodeCompras = [];
+
+const agregaraCarrito = (id) => {
+  let producto = productos.find(producto => producto.id == id)
+
+  if (existeObjetoConID(carritodeCompras, producto.id)) {
+    console.log("Ya existe un objeto con el mismo ID");
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Este producto ya esta en tu carrito',
+      // footer: '<a href="">Why do I have this issue?</a>'
+    })
+  } else {
+    carritodeCompras.push(producto);
+    console.log("Objeto agregado correctamente");
+    Swal.fire({
+      title: 'Producto agregado al carrito',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    })
+  }
+  
+    renderCarritoCompras();
+    actualizarNumeroCarrito(carritodeCompras);
+  
+}
+
+/* -------------------------------------------------------------------------- */
+// renderizar la lista del carrito de compras
+
+const container_carrito = document.querySelector('#products-id');
+
+const renderCarritoCompras = () => {
+  console.log(carritodeCompras);
+  let card_carrito = ''
+
+  carritodeCompras.forEach(producto => {
+    
+    
+    card_carrito += `
+    <div clas = 'card-items'>
+      <div class="item">
+        <img src="${producto.imagen}" alt="${producto.alt}">
+        <div class="item-content">
+        <span>${producto.titulo}</span>
+        <strong class="cart-price">$${producto.precio}v</strong>
+      
+      </div>
+    </div>
+    
+    `
+  })
+  container_carrito.innerHTML = card_carrito;
+
+  renderTotalCarrito(carritodeCompras)
+  
+}
+
+
+
+/* ------------------- numero del productos en el carrito ------------------ */
+
+const numeroCarrito = document.querySelector('.count-product');
+const actualizarNumeroCarrito = (carrito) => {
+  let num = carrito.length;
+  numeroCarrito.textContent = num;
+}
+/* -------------------------------------------------------------------------- */
+
+
+
+/* ------------------------------- suma total ------------------------------- */
+const sumarTotalCarrito = (arrayObjetos) => {
+  let total = 0;
+  arrayObjetos.forEach(element => {
+    total += element.precio;
+    
+  })
+  return total
+
+  
+}
+console.log(sumarTotalCarrito(carritodeCompras));
+ 
+const renderTotalCarrito = (arr) => {
+  const totalTxt = document.querySelector('.carritoTotal__text');
+  totalTxt.textContent = `$ ${sumarTotalCarrito(arr)}`
+}
+
+
+const finalizarCompra = () => {
+  Swal.fire({
+    //title: 'Finalizar compra',
+    text: '¡Gracias por tu compra!',
+    icon: 'success'
+  });
+
+  // Vacía el carrito de compras
+  carritodeCompras = [];
+  renderCarritoCompras();
+  actualizarNumeroCarrito(carritodeCompras);
+
+  // Oculta el botón de "Finalizar compra"
+  const btnFinalizar = document.querySelector('#btn-finalizar-compra');
+  btnFinalizar.classList.add('hidden');
+};
+
+const btnFinalizarCompra = document.querySelector('#btn-finalizar-compra');
+btnFinalizarCompra.addEventListener('click', finalizarCompra);
---
+++ Js/data.js
@@ -0,0 +1,152 @@
+const productos = [
+
+
+   
+  {
+    id: 1,//algo unico
+    titulo: "iPhone 12 Pro Max 256gb Grafito",
+    imagen: "../img/imperdibles1.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "celulares",
+        
+    },
+    precio:  4459999,
+  },
+  {
+    id: 2,//algo unico
+    titulo: "Laptop Asus Vivobook Pro 15 OLED quiet blue 15.6",
+    imagen: "../img/imperdibles2.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "laptos",
+        
+    },
+    precio:  2644900,
+  },
+  {
+    id: 3,//algo unico
+    titulo: "Televisor Samsung 55 Crystal Uhd 4k Cu8200",
+    imagen: "../img/imperdibles3.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "televisores",
+        
+    },
+    precio:  2199900,
+  },
+  {
+    id: 4,//algo unico
+    titulo: "Audífonos gamer Kotion Each G9000 black y blue con luz LED",
+    imagen: "../img/imperdibles4.jpeg",
+    alt: " Iphone max ",
+    detalles : "la descricion",
+
+    categoria: {
+        nombre: "accesorios",
+        
+    },
+    precio:  51443,
+  },
+  {
+    id: 5,//algo unico
+    titulo: "Tablet Amazon Fire HD 10 2021 KFTRWI 10.1 32gb",
+    imagen: "../img/TabletAmazon.jpg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "tablets",
+        
+    },
+    precio:  529900,
+  },
+  {
+    id: 6,//algo unico
+    titulo: "Celular Samsung Galaxy M14 5g Azul Oscuro 6000 Mah",
+    imagen: "../img/SamsungGalaxy.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "celulares",
+        
+    },
+    precio:  849900,
+  },
+  {
+    id: 7,//algo unico
+    titulo: "Todo En Uno Hp 21-b003la Celeron J4025",
+    imagen: "../img/TodoEnUnoHp.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "computadores",
+        
+    },
+    precio:  1286223,
+  },
+  {
+    id: 8,//algo unico
+    titulo: "Smart TV Samsung Series 6 QN60Q65BAKXZL QLED",
+    imagen: "../img/SmartTVSamsung.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "televisores",
+        
+    },
+    precio:  2599900,
+  },
+  {
+    id: 9,//algo unico
+    titulo: "Audífonos inalámbricos Lenovo X4 negro",
+    imagen: "../img/AudífonosInalámbricos.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "accesorios",
+        
+    },
+    precio:  132990,
+  },
+  {
+    id: 10,//algo unico
+    titulo: "Apple iPad (9ª generación) 10.2 Wi-Fi 64GB - Gris",
+    imagen: "../img/AppleiPad.jpg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "tablets",
+        
+    },
+    precio:  1584999,
+  },
+  {
+    id: 11,//algo unico
+    titulo: "Huawei P30 Lite 128 GB midnight black 4 GB RAM",
+    imagen: "../img/HuaweiP30.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "celulares",
+        
+    },
+    precio:  569900,
+  },
+  {
+    id: 12,//algo unico
+    titulo: "Ultrabook Lenovo IdeaPad 14IGL05 platinum gray 14",
+    imagen: "../img/UltrabookLenovo.jpeg",
+    alt: " Iphone max ",
+
+    categoria: {
+        nombre: "celulares",
+        
+    },
+    precio:  1010676,
+  },
+
+]
+
