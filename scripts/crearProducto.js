d8ec73ebb0f5650d93838e81bcd835e15213ef39
Hackathon <ktyneth@hotmail.com>
2023-07-24 14:28:28 -0500

    formulario crear

--- scripts/crearProducto.js
+++ scripts/crearProducto.js
@@ -10,6 +10,7 @@ menuToggle.addEventListener('click', function(){        //Abre el menu
     body.classList.toggle('open');
 })
 
+//formularios
 const signupForm = document.querySelector('.signup-form')
 
 signupForm.addEventListener('submit', (e) => {
@@ -17,23 +18,24 @@ signupForm.addEventListener('submit', (e) => {
 
     //Referenciar los datos
     const name = document.querySelector('#name').value
-    const actividadEconomica = document.querySelector('#act-econo').value
-    const email = document.querySelector('#email').value
-    const telefono = document.querySelector('#telefono').value
-    const direccion = document.querySelector('#direccion').value
+    const categoria = document.querySelector('#categoria').value
+    const precio = document.querySelector('#precio').value
+    const cantidad = document.querySelector('#cantidad').value
+    const descripcion = document.querySelector('#descripcion').value
     const imagen = document.querySelector('#imagenProducto').value
-    const password = document.querySelector('#password').value
-    //const passwordConf = document.querySelector('#password-confirm').value
+    const vendedor = document.querySelector('#vendedor').value
 
-    const url = 'http://localhost:8080/CrudRepo/crearSeller';
+    const url = 'http://localhost:8080/product/crearProduct';
     let data ={
-      nameSeller: name,
-      activitySeller: actividadEconomica,
-      addressSeller: direccion,
-      emailSeller:email,
-      phoneSeller: telefono,
-      passwordSeller: password,
-      imageSeller:imagen
+      nameProduct: name,
+      category: categoria,
+      price: precio,
+      quantity:cantidad,
+      description: descripcion,
+      imageProduct: imagen,
+      seller: {
+        idSeller: vendedor
+    }
   }
   let fetchData ={
     method: 'POST',
@@ -49,7 +51,7 @@ fetch(url, fetchData)
     // Aquí puedes hacer lo que necesites con la respuesta del servidor (si aplica)
   })
   .catch(error => {
-    console.error('Error al guardar los datos:', error);    
+    console.error('Error al guardar los datos:', error);
     // Manejar el error (si aplica)
   });
-});
+});  
